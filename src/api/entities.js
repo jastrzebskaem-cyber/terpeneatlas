9bee7513181cdef3ea337abe0192c97bf5a85070
jastrzebskaem-cyber <jastrzebska.em@gmail.com>
2026-04-15 21:31:42 +0200

    fix: update terpene atlas (src/api/entities.js)

---
+++ src/api/entities.js
@@ -0,0 +1,7 @@
+import { base44 } from './base44Client';
+
+export const Review = base44.entities.Review;
+export const Favorite = base44.entities.Favorite;
+
+// auth sdk:
+export const User = base44.auth;
